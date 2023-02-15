f3691b70c3b29838c131fb7af9a4bc48a7f9d2e1
David Holloway <dave_41266@hotmail.com>
2023-02-14 18:56:13 -0600

    initial commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,35 @@
+import React from "react";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby palo santo man braid butcher, fashion axe stumptown
+            williamsburg ethical truffaut PBR&B. Hella tattooed franzen yes plz
+            flannel bushwick raw denim scenester viral swag yuccie ugh
+            church-key same.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
---
+++ server.js
@@ -0,0 +1,39 @@
+import "express-async-errors";
+import express from "express";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
+const app = express();
+dotenv.config();
+
+// makes json data available in controllers
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
